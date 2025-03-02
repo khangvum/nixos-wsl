2b7597268546d5cd64f5cdddcc6799bd3c89b4db
khangvum <manhkhang0305@gmail.com>
2025-03-02 21:45:29 +0000

    Remove packages and apps

--- flake.nix
+++ flake.nix
@@ -20,15 +20,6 @@
       # Aliases      
       lib = nixpkgs.lib;
       pkgs = nixpkgs.legacyPackages.${system};
-
-      # Systems that can run tests
-      supportedSystems = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
-
-      # Function to generate a set based on supported systems
-      forAllSystems = inputs.nixpkgs.lib.genAttrs supportedSystems;
-
-      # Attribute set of nixpkgs for each system
-      nixpkgsFor = forAllSystems (system: import inputs.nixpkgs { inherit system; });
     in {
     nixosConfigurations = {
       nixos = lib.nixosSystem {
@@ -58,28 +49,5 @@
         };
       };
     };
-
-    packages = forAllSystems (system:
-      let 
-        pkgs = nixpkgsFor.${system};
-      in {
-        default = self.packages.${system}.installation;
-
-        installation = pkgs.writeShellApplication {
-          name = "installation";
-          runtimeInputs = with pkgs; [ git ];
-          text = ''${./installation.sh} "$@"'';
-          };
-      }
-    );
-
-    apps = forAllSystems (system: {
-      default = self.apps.${system}.installation;
-
-      installation = {
-        type = "app";
-        program = "${self.packages.${system}.installation}/bin/installation";
-      };
-    });
   };
 }
--- installation.sh
+++ installation.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
 
-# Clone the dotfiles
-if [ $# -gt 0 ]
-  then
-    SCRIPT_DIR=$1
-  else
-    SCRIPT_DIR=~/.dotfiles
-fi
-nix-shell -p git --command "git clone https://github.com/khangvum/nixos-wsl.git $SCRIPT_DIR"
-
-# Patch flake.nix and home.nix with the host's username
-sed -i "0,/khangvum/s//$(whoami)/" $SCRIPT_DIR/flake.nix
-sed -i "0,/khangvum/s//$(whoami)/" $SCRIPT_DIR/home.nix
-sed -i "s/manhkhang0305@gmail.com//" $SCRIPT_DIR/home.nix
-# - Replaces all occurrences of ~/.dotfiles with the actual SCRIPT_DIR path
-sed -i "s+~/.dotfiles+$SCRIPT_DIR+g" $SCRIPT_DIR/flake.nix
-
-# Open flake.nix for manual modification before installation
-if [ -z "$EDITOR" ]; then
-    EDITOR=nano;
-fi
-$EDITOR $SCRIPT_DIR/flake.nix;
-
 # Apply system-wide configuration
-sudo nixos-rebuild switch --flake $SCRIPT_DIR;
+sudo nixos-rebuild switch --flake ~/.dotfiles;
 
 # Apply user-specific configuration
-nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake $SCRIPT_DIR;
+home-manager switch --flake ~/.dotfiles;
