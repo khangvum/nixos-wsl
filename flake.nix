34ad4cb4afca17b3fbdae9c564db4c62806d04bd
khangvum <manhkhang0305@gmail.com>
2025-03-02 19:52:26 +0000

    Add packages and apps

--- flake.nix
+++ flake.nix
@@ -20,6 +20,15 @@
       # Aliases      
       lib = nixpkgs.lib;
       pkgs = nixpkgs.legacyPackages.${system};
+
+      # Systems that can run tests
+      supportedSystems = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
+
+      # Function to generate a set based on supported systems
+      forAllSystems = inputs.nixpkgs.lib.genAttrs supportedSystems;
+
+      # Attribute set of nixpkgs for each system
+      nixpkgsFor = forAllSystems (system: import inputs.nixpkgs { inherit system; });
     in {
     nixosConfigurations = {
       nixos = lib.nixosSystem {
@@ -49,5 +58,28 @@
         };
       };
     };
+
+    packages = forAllSystems (system:
+      let 
+        pkgs = nixpkgsFor.${system};
+      in {
+        default = self.packages.${system}.installation;
+
+        installation = pkgs.writeShellApplication {
+          name = "installation";
+          runtimeInputs = with pkgs; [ git ];
+          text = ''${./installation.sh} "$@"'';
+          };
+      }
+    );
+
+    apps = forAllSystems (system: {
+      default = self.apps.${system}.installation;
+
+      installation = {
+        type = "app";
+        program = "${self.packages.${system}.installation}/bin/installation";
+      };
+    });
   };
 }
