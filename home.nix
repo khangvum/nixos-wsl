822a27b9567030ef6d4df28bdd74304d5f3dfec0
khangvum <manhkhang0305@gmail.com>
2025-03-07 04:08:13 +0000

    Add alias commands

--- home.nix
+++ home.nix
@@ -74,11 +74,33 @@
   programs.bash = {
     enable = true;
     shellAliases = {
-      "ll" = "ls -l";
+      # - ~/.dotfiles
+      ".dotfiles"="cd ~/.dotfiles";
+
+      # - cd
       ".." = "cd ..";
-      ".dotfiles" = "cd ~/.dotfiles";
+      "..." = "cd ../../../";
+      "...." = "cd ../../../../";
+      "....." = "cd ../../../../";
+      ".4" = "cd ../../../../";
+      ".5" = "cd ../../../../..";
+
+      # - clear
+      "c" = "clear";
+      "cls" = "clear";
+
+      # - sudo
+      "su" = "sudo -i";
+      "root" = "sudo -i";
+
+      # - reboot/poweroff/halt/shutdown
+      "reboot" = "sudo reboot";
+      "poweroff" = "sudo poweroff";
+      "halt" = "sudo halt";
+      "shutdown" ="sudo shutdown";
     };
     initExtra = ''
+      # Change the default directory to user's home directory
       cd ~
     '';
   };
