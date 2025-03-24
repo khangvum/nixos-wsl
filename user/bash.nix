1fd2bc0cb3564b09d054f2951c3e9ca01edafd0d
khangvum <manhkhang0305@gmail.com>
2025-03-24 01:16:36 +0000

    Add bash.nix

---
+++ user/bash.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, ... }:
+
+{
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      # - ~/.dotfiles
+      ".dotfiles"="cd ~/.dotfiles";
+
+      # - cd
+      ".." = "cd ..";
+      "..." = "cd ../../../";
+      "...." = "cd ../../../../";
+      "....." = "cd ../../../../";
+      ".4" = "cd ../../../../";
+      ".5" = "cd ../../../../..";
+
+      # - clear
+      "c" = "clear";
+      "cls" = "clear";
+
+      # - sudo
+      "su" = "sudo -i";
+      "root" = "sudo -i";
+
+      # - reboot/poweroff/halt/shutdown
+      "reboot" = "sudo reboot";
+      "poweroff" = "sudo poweroff";
+      "halt" = "sudo halt";
+      "shutdown" ="sudo shutdown";
+    };
+    initExtra = ''
+    
+      # Change the default directory to user's home directory
+      cd ~
+    '';
+  };
+}
