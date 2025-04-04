8441a26de6453d651fc4fd9fdd83a3340f60e1b1
khangvum <manhkhang0305@gmail.com>
2025-04-04 01:51:26 +0000

    Updated Home Manager installation script

--- installation.sh
+++ installation.sh
@@ -6,14 +6,13 @@ DOTFILES_PATH="/etc/nixos/.dotfiles"
 # Apply system-wide configuration
 sudo nixos-rebuild switch --flake $DOTFILES_PATH
 
-# Check if Home Manager is installed
-if ! command -v home-manager &> /dev/null; then
-    echo "Home Manager is not installed. Installing..."
+# Check if Home Manager is already installed
+if command -v home-manager &> /dev/null; then
+    echo "Home Manager is already installed."
+else
     nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz home-manager
     nix-channel --update
     nix-shell '<home-manager>' -A install
-else
-    echo "Home Manager is already installed."
 fi
 
 # Apply user-specific configuration
