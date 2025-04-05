7d4e8218d17085a9e30d9f92ec867ffa177594de
khangvum <manhkhang0305@gmail.com>
2025-04-05 00:25:46 +0000

    Added Bash color code

--- installation.sh
+++ installation.sh
@@ -8,8 +8,9 @@ sudo nixos-rebuild switch --flake $DOTFILES_PATH
 
 # Check if Home Manager is already installed
 if command -v home-manager &> /dev/null; then
-    echo "Home Manager is already installed."
+    echo -e "\e[32mHome Manager is already installed\e[0m"
 else
+    echo -e "\e[33mInstalling Home Manager\e[0m"
     nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz home-manager
     nix-channel --update
     nix-shell '<home-manager>' -A install
